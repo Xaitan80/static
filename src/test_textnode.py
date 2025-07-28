abd0b1244f794d88bf1a69c439b7d0e488ed0aa0
Xaitan <utrustad.sprang_0z@icloud.com>
2025-07-28 11:39:17 +0200

    updated to same as boot.dev to not have issues going forward

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -5,26 +5,31 @@ from textnode import TextNode, TextType
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", TextType.BOLD)
-        node2 = TextNode("This is a text node", TextType.BOLD)
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.TEXT)
         self.assertEqual(node, node2)
 
-    def test_not_equal_different_url(self):
-        node1 = TextNode("Sample text", TextType.LINK, "http://example.com")
-        node2 = TextNode("Sample text", TextType.LINK, "http://different.com")
-        self.assertNotEqual(node1, node2)  # Should pass
+    def test_eq_false(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
 
-    def test_eq_same_properties(self):
-        node1 = TextNode("Sample text", TextType.LINK, "http://example.com")
-        node2 = TextNode("Sample text", TextType.LINK, "http://example.com")
-        self.assertEqual(node1, node2)  # Should pass
+    def test_eq_false2(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node2", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_url(self):
+        node = TextNode("This is a text node", TextType.ITALIC, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", TextType.ITALIC, "https://www.boot.dev")
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
 
 
-    def test_not_equal_different_text_type(self):
-        node1 = TextNode("Sample text", TextType.BOLD)
-        node2 = TextNode("Sample text", TextType.ITALIC)
-        self.assertNotEqual(node1, node2)  # Should pass
-
-        
 if __name__ == "__main__":
     unittest.main()
