9b556a4e86f03d465e403b785068109e01666e61
Xaitan <utrustad.sprang_0z@icloud.com>
2025-07-30 12:23:44 +0200

    stuff

--- src/my_regex.py
+++ src/my_regex.py
@@ -1,4 +1,6 @@
 import re
+from htmlnode import LeafNode, ParentNode
+from textnode import TextNode, TextType
 
 def extract_markdown_images(text):
     # Regex for markdown image: ![alt](url)
@@ -21,24 +23,11 @@ def extract_markdown_links(text):
 
 def split_nodes_image(old_nodes):
     results = []
-    i = 0
-    while i < len(old_nodes):
-        if old_nodes[i:i+2] == "![":
-            alt_start = i + 2
-            alt_end = old_nodes.find("]", alt_start)
-            if alt_end == -1:
-                break
-            if old_nodes[alt_end + 1] != "(":
-                i = alt_end + 1
-                continue
-            url_start = alt_end + 2
-            url_end = old_nodes.find(")", url_start)
-            if url_end == -1:
-                break
-                alt_text = old_nodes[alt_start:alt_end]
-                url = text[url_start:url_end]
-                results.append((alt_text, url))
-                i = url_end + 1
-            else:
-                i += 1
-        return results
+    for node in old_nodes:
+        if node.text_type is not TextType.TEXT:
+            results.append(node)
+        else:
+            results.append(node)
+
+    return results
+
