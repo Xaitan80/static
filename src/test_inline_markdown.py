5e41f3e88ec3c7ad23ca320fb9a243ba73ca5da8
Xaitan <utrustad.sprang_0z@icloud.com>
2025-07-31 09:40:17 +0200

    alligned files with course

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,5 +1,7 @@
 import unittest
-from my_regex import extract_markdown_images, extract_markdown_links
+from inline_markdown import extract_markdown_images, extract_markdown_links, split_nodes_image
+from htmlnode import LeafNode, ParentNode
+from textnode import TextNode, TextType
 
 class TestMarkdownExtraction(unittest.TestCase):
     def test_extract_single_image(self):
@@ -38,5 +40,37 @@ class TestMarkdownExtraction(unittest.TestCase):
         text = "This is a link: [click](http://link.com)"
         self.assertEqual(extract_markdown_images(text), [])
 
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+        [
+            TextNode("This is text with an ", TextType.TEXT),
+            TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" and another ", TextType.TEXT),
+            TextNode(
+                "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+            ),
+        ],
+        new_nodes,
+    )
+    def test_split_one_image(self):
+        node = TextNode(
+            "This is a text with an ![image](https://i.imgur.com/zjjcJKZ.png)",
+            TextType.TEXT,
+
+        )
+        new_node = split_nodes_image([node])
+        self.assertListEqual(
+        [
+            TextNode("This is a text with an ", TextType.TEXT),
+            TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png")
+        ],
+        new_node
+    )
 if __name__ == '__main__':
     unittest.main()
