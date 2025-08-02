aadde6590f9c509769cee1783994e57c5e09145e
Xaitan <utrustad.sprang_0z@icloud.com>
2025-08-02 21:47:31 +0200

    synced files with lesson

--- src/main.py
+++ src/main.py
@@ -1,65 +1,26 @@
 import os
 import shutil
-from markdown_blocks import markdown_to_html_node
 
+from copystatic import copy_files_recursive
+from gencontent import generate_pages_recursive
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 
 def main():
-    copy_static_files("static", "public")
-    print("Static files copied successfully!")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-def copy_static_files(src="static", dest="public"):
-    if os.path.exists(dest):
-        shutil.rmtree(dest)
-        print(f"removed exsisting derectory {dest}")
-    os.mkdir(dest)
-    print(f"created directory {dest}")
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
-    items = os.listdir(src)
-    for item in items:
-        src_path = os.path.join(src, item)
-        dest_path = os.path.join(dest, item)
-        if os.path.isfile(src_path):
-            shutil.copy(src_path, dest_path)
-            print (f"copied files from {src_path} to {dest_path}")
-        else:
-            os.mkdir(dest_path)
-            print(f"created directory {dest_path}")
-            copy_static_files(src_path, dest_path)
-
-def extract_title(markdown):
-    lines = markdown.split("\n")
-    for line in lines:
-        if line.startswith("# "):
-            return line[2:].strip()
-    raise Exception("No level-one header found")
-
-def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
-    with open(from_path, "r") as f:
-        markdown_content = f.read()
-    with open(template_path, "r") as f:
-        template = f.read()
-        
-    title = extract_title(markdown_content)
-    html = markdown_to_html_node(markdown_content).to_html()
-    final_html = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
-
-    #check the filepath
-    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-
-    with open(dest_path, "w") as f:
-        f.write(final_html)
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
 
-
-
-
-
-            
-
-    
-
-if __name__ == "__main__":
-    main()
+main()
--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,4 +1,5 @@
 from enum import Enum
+
 from htmlnode import ParentNode
 from inline_markdown import text_to_textnodes
 from textnode import text_node_to_html_node, TextNode, TextType
@@ -51,7 +52,6 @@ def block_to_block_type(block):
     return BlockType.PARAGRAPH
 
 
-# putting it all together
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
     children = []
@@ -59,6 +59,7 @@ def markdown_to_html_node(markdown):
         html_node = block_to_html_node(block)
         children.append(html_node)
     return ParentNode("div", children, None)
+
 
 def block_to_html_node(block):
     block_type = block_to_block_type(block)
@@ -74,7 +75,8 @@ def block_to_html_node(block):
         return ulist_to_html_node(block)
     if block_type == BlockType.QUOTE:
         return quote_to_html_node(block)
-    raise ValueError("invalid block type") 
+    raise ValueError("invalid block type")
+
 
 def text_to_children(text):
     text_nodes = text_to_textnodes(text)
@@ -83,7 +85,8 @@ def text_to_children(text):
         html_node = text_node_to_html_node(text_node)
         children.append(html_node)
     return children
-    
+
+
 def paragraph_to_html_node(block):
     lines = block.split("\n")
     paragraph = " ".join(lines)
@@ -93,24 +96,26 @@ def paragraph_to_html_node(block):
 
 def heading_to_html_node(block):
     level = 0
-    while level < len(block) and block[level] == "#":
-        level +=1
-    if level < 1 or level > 6:
-        raise ValueError ("Invalid heading level")
-    heading_text = block[level:].strip()
-    children = text_to_children(heading_text)
-
+    for char in block:
+        if char == "#":
+            level += 1
+        else:
+            break
+    if level + 1 >= len(block):
+        raise ValueError(f"invalid heading level: {level}")
+    text = block[level + 1 :]
+    children = text_to_children(text)
     return ParentNode(f"h{level}", children)
+
 
 def code_to_html_node(block):
     if not block.startswith("```") or not block.endswith("```"):
-        raise ValueError ("invalid code block")
+        raise ValueError("invalid code block")
     text = block[4:-3]
     raw_text_node = TextNode(text, TextType.TEXT)
     child = text_node_to_html_node(raw_text_node)
-    code_node = ParentNode("code", [child])
-    return ParentNode("pre", [code_node])
-
+    code = ParentNode("code", [child])
+    return ParentNode("pre", [code])
 
 
 def olist_to_html_node(block):
@@ -122,6 +127,7 @@ def olist_to_html_node(block):
         html_items.append(ParentNode("li", children))
     return ParentNode("ol", html_items)
 
+
 def ulist_to_html_node(block):
     items = block.split("\n")
     html_items = []
@@ -131,23 +137,14 @@ def ulist_to_html_node(block):
         html_items.append(ParentNode("li", children))
     return ParentNode("ul", html_items)
 
+
 def quote_to_html_node(block):
     lines = block.split("\n")
     new_lines = []
     for line in lines:
         if not line.startswith(">"):
-            raise ValueError ("Invalid quote block")
-        line = line[1:].lstrip()
-        new_lines.append(line)
+            raise ValueError("invalid quote block")
+        new_lines.append(line.lstrip(">").strip())
     content = " ".join(new_lines)
     children = text_to_children(content)
     return ParentNode("blockquote", children)
-
-
-
-
-
-
-   
-
-        
