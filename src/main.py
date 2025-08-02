d1af9daf822ff13b2e4dfebfb5963dcaed1550db
Xaitan <utrustad.sprang_0z@icloud.com>
2025-08-02 18:39:14 +0200

    file copy done

--- src/main.py
+++ src/main.py
@@ -1,10 +1,35 @@
-from textnode import TextNode, TextType
+###from textnode import TextNode, TextType
 #from delimiters import split_nodes_delimiter
 
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    copy_static_files("static", "public")
+    print("Static files copied successfully!")
 
+import os
+import shutil
 
-main()
+def copy_static_files(src="static", dest="public"):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+        print(f"removed exsisting derectory {dest}")
+    os.mkdir(dest)
+    print(f"created directory {dest}")
+
+    items = os.listdir(src)
+    for item in items:
+        src_path = os.path.join(src, item)
+        dest_path = os.path.join(dest, item)
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dest_path)
+            print (f"copied files from {src_path} to {dest_path}")
+        else:
+            os.mkdir(dest_path)
+            print(f"created directory {dest_path}")
+            copy_static_files(src_path, dest_path)
+            
+
+    
+
+if __name__ == "__main__":
+    main()
